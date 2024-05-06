06542a001168d15e2a8a0c25b889378b3e9f522f
VijayaDharshiniSivakumar <128115444+VijayaDharshiniSivakumar@users.noreply.github.com>
2024-05-06 13:38:13 +0530

    Update regression.java

--- regression.java
+++ regression.java
@@ -6,11 +6,11 @@ import org.junit.Test;
 public class RegressionTest {
     @Test
     public void testAddition() {
-        assertEquals(8, Main.add(3, 5));
+        assertEquals(8, Main.add(10, 5));
     }
 
     @Test
     public void testSubtraction() {
-        assertEquals(5, Main.subtract(10, 5));
+        assertEquals(5, Main.subtract(12, 5));
     }
 }
